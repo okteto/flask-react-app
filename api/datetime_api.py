b680118ffebaab48ed3c47bbb2f11b2de1aeab1c
Muhammed Ali <muhamzyali@gmail.com>
2021-06-26 04:06:07 +0100

    api

--- api/datetime_api.py
+++ api/datetime_api.py
@@ -3,8 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/datetime')
+@app.route('/datetime', methods=['GET'])
 def get_current_time():
-    return {'Date and Time is': datetime.now()}
+  return {'datetime': datetime.now()}
 
-
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0')
